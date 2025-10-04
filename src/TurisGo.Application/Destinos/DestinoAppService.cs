43e4722b1547b79e654db47310d07e384503fe8d
Juan Menoyo <juanmenoyo11@gmail.com>
2025-10-03 23:23:10 -0300

    Correcciones: Validaciones + test con DbContext

--- src/TurisGo.Application/Destinos/DestinoAppService.cs
+++ src/TurisGo.Application/Destinos/DestinoAppService.cs
@@ -24,39 +24,7 @@ namespace TurisGo.Destinos
 
     {
         public DestinoAppService(IRepository<Destino, Guid> repository) : base(repository) { }
-
-
-        public override async Task<DestinoDto> CreateAsync(CreateUpdateDestinoDto input)
-        {
-         
-            //Validacion de negocio: No duplicar destinos
-            var existe = await Repository.AnyAsync(
-                d => d.Nombre == input.Nombre && d.Pais == input.Pais
-                );
-
-            if (existe)
-            {
-                throw new BusinessException("Destino.Duplicado")
-                    .WithData("Nombre", input.Nombre)
-                    .WithData("Pais", input.Pais);
-            }
-
-            //No duplicar coordenadas
-            var igualCoord = await Repository.AnyAsync(
-                d => d.Coordenada.Latitud == input.Coordenada.Latitud &&
-                     d.Coordenada.Longitud == input.Coordenada.Longitud
-            );
-
-            if (igualCoord)
-            {
-                throw new BusinessException("Destino.CoordenadaDuplicada");
-            }
-
-            return await base.CreateAsync(input);
-
-        
-        }
-
     }
-        
+
 }
+        
